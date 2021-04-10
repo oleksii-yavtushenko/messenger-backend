15422aed7423d7cede9771c31b102d90504660b4
volvozachto <fogmrfog@gmail.com>
2021-04-10 16:22:49 +0300

    Add executable-jar package on the client-side

--- ClientSide/src/main/java/com/letter/Main.java
+++ ClientSide/src/main/java/com/letter/Main.java
@@ -33,4 +33,8 @@ public class Main extends Application {
             }
         });
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ ClientSide/src/main/java/com/letter/MainLauncher.java
@@ -0,0 +1,8 @@
+package com.letter;
+
+
+public class MainLauncher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
